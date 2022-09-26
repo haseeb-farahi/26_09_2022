d42b85d6d124e2bbb35612ea910821b15208f02d
haseeb-farahi <111962155+haseeb-farahi@users.noreply.github.com>
2022-09-26 11:52:23 +0430

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,31 @@
+
+// $('#theP1').click(()=>{
+//     $('#theBall').animate({left: '1100px'},2000)
+// })
+
+
+// $('#theP2').click(()=>{
+//     $('#theBall').animate({left: '0px'},2000)
+// })
+
+$('button').click(()=>{
+    // $('p').remove('#test, .test');
+    // $('div').empty();
+    // document.querySelector('div').classList.add('parnt')
+
+    // $('div,ul,li,span').toggleClass('class')
+//     $('div').css({'color':"gray",'font-size': '18px'})
+// $('button').css('background-color',"red").addClass('btn btn-info')
+// $('h1').clone().appendTo('span')
+
+// $('small').wrapInner('<mark></mark>')
+$('div').children('p:first').css({'border':'2px solid red', 'color':'red'})
+// $('').parents('').css({'border':'2px solid red', 'color':'red'})
+
+
+})
+// $('#rmClass').click(()=>{
+//     $('span').('parnt')
+// })
+
+
